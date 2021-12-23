66efa6b3f9836ffb158ef431d105b5a05044e336
Omar Sánchez Avelar <omar_sanchez_avelar@hotmail.com>
2021-12-22 22:14:46 -0600

    se agrega funcionalidad con js al faq

---
+++ js/faq.js
@@ -0,0 +1,43 @@
+let question = document.getElementsByClassName('faq-list__item--button');
+let answer = document.querySelectorAll('.answer');
+let cont = 0;
+
+for(let i = 0; i< question.length; i++)
+{
+   question[i].addEventListener('click', () =>
+   {
+      if(cont > 0 && position != i)
+      {
+         hideShow(position);
+         showHide(i);
+         position = i;
+      }
+
+      else 
+      {
+         if(answer[i].classList.contains('open'))
+         {
+            hideShow(i, position);            
+            cont = 0
+         }
+         else 
+         {
+            showHide(i);
+            cont ++;
+            position = i;
+         }
+      }
+   });
+}
+
+function hideShow (b)
+{
+   answer[b].classList.remove('open');
+   answer[b].classList.add('closed');
+}
+
+function showHide (a)
+{
+   answer[a].classList.add('open');
+   answer[a].classList.remove('closed');
+}
